cbb2999eb4abba31f979c12e15e239f3254e77f4
Vinícius de Freitas <viniciusfreitas.1.4.4@gmail.com>
2022-07-05 23:12:24 -0300

    feat: creating the auth model, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import dbConfig from './config/ormconfig';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(dbConfig as TypeOrmModuleOptions),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
